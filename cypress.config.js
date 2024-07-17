4e85b39985fe213192c135c2c2323f715cc68e4a
Raghav-539 <rg0256539@gmail.com>
2024-07-17 20:18:41 +0530

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   
   reporter: 'cypress-mochawesome-reporter',  // generating html reports
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
